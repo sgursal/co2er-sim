8375d22a4b0f1672c93bb6595affbc43ae3c6158
sgursal <shreyagursal@gmail.com>
2026-06-07 16:18:29 +0100

    Finalised folder structure

--- tests/test_kinetics.py
+++ tests/test_kinetics.py
@@ -6,6 +6,5 @@ from src.kinetics.butler_volmer import compute_current_density
 
 
 def test_bv_zero_overpotential():
-    i = compute_current_density(1.0, 0.5, 0.5, 0.5, 0, 298, 1)
+    i = compute_current_density(0.0, 0.5, 0.5, 0.5, 298, 2)
     assert abs(i) < 1e-12
-    pass
